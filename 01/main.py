8107e9c118394d4c08231e248d485cf4de1618a1
Lesly Kinzel <lesly.helios@proton.me>
2024-12-01 23:00:42 +1100

    Complete day 1.
    Solution: 1580061

--- 01/main.py
+++ 01/main.py
@@ -1,9 +1,14 @@
+import pandas as pd
 
 def main():
-    pass
+    df = pd.read_csv('input.txt', sep=r'\s+', header=None, engine='python')
 
-def get_distance(x: int, y: int) -> int:
-    return abs(x - y)
+    col_1 = sorted(df[0])
+    col_2 = sorted(df[1])
+
+    result = [abs(c1 - c2) for c1, c2 in zip(col_1, col_2)]
+
+    print(sum(result))
 
 if __name__ == '__main__':
     main()

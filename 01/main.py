34b507e1faeef58657ebd76a345b0ece7d56db60
Lesly Kinzel <lesly.helios@proton.me>
2024-12-01 22:30:43 +1100

    Replace if block with abs()

--- 01/main.py
+++ 01/main.py
@@ -3,9 +3,7 @@ def main():
     pass
 
 def get_distance(x: int, y: int) -> int:
-    if x >= y:
-        return x - y
-    return y - x
+    return abs(x - y)
 
 if __name__ == '__main__':
     main()

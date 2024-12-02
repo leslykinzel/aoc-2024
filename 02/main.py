d85aba3b18b37dc3993fcaff0f8ccaa3690ed4f0
Lesly Kinzel <lesly.helios@proton.me>
2024-12-02 17:32:13 +1100

    Some whitespace for my sanity.

--- 02/main.py
+++ 02/main.py
@@ -5,18 +5,22 @@ def main():
         data = [line.split() for line in file.readlines()]
 
     longest_line = max(len(row) for row in data)
+
     for i in range(len(data)):
         while len(data[i]) < longest_line:
             data[i].append(None)
 
     df = pd.DataFrame(data)
+
     for i, r in df.iterrows():
         print(is_ordered_and_unique(r))
 
 def is_ordered_and_unique(l: list) -> bool:
     l = [int(x) for x in l if x is not None]
+
     ordered = l == sorted(l) or l == sorted(l, reverse=True)
     unique = len(l) == len(set(l))
+
     return ordered and unique
 
 def remove_item(l: list, s: str) -> list:

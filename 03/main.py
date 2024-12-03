e85d0e7ab1e29c44e51107b70c04ec09a6f15394
Lesly Kinzel <lesly.helios@proton.me>
2024-12-04 00:14:26 +1100

    Remove unnecessary assignment.

--- 03/main.py
+++ 03/main.py
@@ -7,8 +7,7 @@ def main():
     with open('input.txt', 'r') as file:
         data = file.read()
 
-    search = MULT_EXPRESSION.finditer(data)
-    for match in search:
+    for match in MULT_EXPRESSION.finditer(data):
         result.append(int(match.group(1)) * int(match.group(2)))
 
     print('Part 1:', sum(result))

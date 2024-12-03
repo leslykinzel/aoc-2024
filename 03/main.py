74ce5d1b362d8b0d605061ce3e8213ac86f8a055
Lesly Kinzel <lesly.helios@proton.me>
2024-12-04 00:15:18 +1100

    Move result.

--- 03/main.py
+++ 03/main.py
@@ -3,9 +3,10 @@ import re
 MULT_EXPRESSION = re.compile(r'mul\((\d+),(\d+)\)')
 
 def main():
-    result = []
     with open('input.txt', 'r') as file:
         data = file.read()
+
+    result = []
 
     for match in MULT_EXPRESSION.finditer(data):
         result.append(int(match.group(1)) * int(match.group(2)))

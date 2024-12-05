7aba5d777472a2c729bdea5efa9f02ecd1a8edf8
Lesly Kinzel <lesly.helios@proton.me>
2024-12-05 19:34:35 +1100

    Complete day 4, part 1.

--- 04/main.py
+++ 04/main.py
@@ -1,18 +1,40 @@
 
 def main():
     with open('input.txt', 'r') as file:
-        # Need strip() to remove newline characters from map.
         data = [list(line.strip()) for line in file]
 
     rows = len(data)
     cols = len(data[0])
-    
-    for i in range(rows):
-        for j in range(cols):
-            pass
+    xmas = 0
 
-def out_of_bounds(x: int, y: int, n_rows: int, n_cols: int) -> bool:
-    return x >= 0 and x <= n_rows and y >= 0 and y <= n_cols
+    for r in range(rows):
+        for c in range(cols):
+            # Horizontal, normal
+            if c + 3 < cols and data[r][c] == 'X' and data[r][c+1] == 'M' and data[r][c+2] == 'A' and data[r][c+3] == 'S':
+                xmas += 1
+            # Horizontal, backwards
+            if c + 3 < cols and data[r][c] == 'S' and data[r][c+1] == 'A' and data[r][c+2] == 'M' and data[r][c+3] == 'X':
+                xmas += 1
+            # Vertical, normal
+            if r + 3 < rows and data[r][c] == 'X' and data[r+1][c] == 'M' and data[r+2][c] == 'A' and data[r+3][c] == 'S':
+                xmas += 1
+            # Vertical, backwards
+            if r + 3 < rows and data[r][c] == 'S' and data[r+1][c] == 'A' and data[r+2][c] == 'M' and data[r+3][c] == 'X':
+                xmas += 1
+            # Diagonal, NE
+            if r + 3 < rows and c + 3 < rows and data[r][c] == 'X' and data[r+1][c+1] == 'M' and data[r+2][c+2] == 'A' and data[r+3][c+3] == 'S':
+                xmas += 1
+            # Diagonal, SW
+            if r + 3 < rows and c + 3 < rows and data[r][c] == 'S' and data[r+1][c+1] == 'A' and data[r+2][c+2] == 'M' and data[r+3][c+3] == 'X':
+                xmas += 1
+            # Diagonal, SE
+            if r - 3 >= 0 and c + 3 < cols and data[r][c] == 'S' and data[r-1][c+1] == 'A' and data[r-2][c+2] == 'M' and data[r-3][c+3] == 'X':
+                xmas += 1
+            # Diagonal, NW
+            if r - 3 >= 0 and c + 3 < cols and data[r][c] == 'X' and data[r-1][c+1] == 'M' and data[r-2][c+2] == 'A' and data[r-3][c+3] == 'S':
+                xmas += 1
+
+    print(xmas)
 
 if __name__ == '__main__':
     main()

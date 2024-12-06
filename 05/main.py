526e386db84d61efa13815e9634baad4f2eb7df1
Lesly Kinzel <lesly.helios@proton.me>
2024-12-06 21:04:21 +1100

    Complete day 5, part 1.

--- 05/main.py
+++ 05/main.py
@@ -1,7 +1,32 @@
+from collections import defaultdict
 
-# X|Y == X must appear before Y
 def main():
-    pass
+    with open('input.txt', 'r') as file:
+        rules, jobs = file.read().strip().split('\n\n')
+
+    part1 = 0
+    part2 = 0
+
+    rules = [tuple(map(int, l.split('|'))) for l in rules.splitlines()]
+    jobs = [tuple(map(int, l.split(','))) for l in jobs.splitlines()]
+
+    ''' Part 1 - Sum middle values in correctly ordererd lists '''
+
+    invalid_ordering = defaultdict(bool)
+    for x, y in rules:
+        invalid_ordering[y,x] = True
+
+    def check_job(job):
+        for i in range(len(job)):
+            for j in range(i+1, len(job)):
+                if invalid_ordering[job[i], job[j]]:
+                    return 0
+        return job[len(job)//2]
+
+    for job in jobs:
+        part1 += check_job(job)
+
+    print('Part 1:', part1)
 
 if __name__ == '__main__':
     main()

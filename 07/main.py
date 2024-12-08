1327a220ce524040753dd714dbb92e17dbdb3b76
Lesly Kinzel <lesly.helios@proton.me>
2024-12-08 15:40:05 +1100

    Taking a break.

--- 07/main.py
+++ 07/main.py
@@ -1,5 +1,18 @@
+from itertools import permutations
 
 def main():
+
+    data = {}
+
+    with open('input.txt', 'r') as file:
+        for line in file:
+            lr = line.strip().split(':')
+            data[int(lr[0].strip())] = list(map(int, lr[1].strip().split()))
+
+    for final_num, factors in data.items():
+        required_operators = len(factors)-1
+
+def eval(expression: str):
     pass
 
 if __name__ == '__main__':

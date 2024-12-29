3228f7d6630f3d69c5b69d946f5ad767c1202bc4
Lesly Kinzel <lesly.helios@proton.me>
2024-12-29 23:50:13 +1100

    Calculate permutations

--- 07/main.py
+++ 07/main.py
@@ -1,19 +1,51 @@
-'''
-190: 10 19
-3267: 81 40 27
-83: 17 5
-156: 15 6
-7290: 6 8 6 15
-161011: 16 10 13
-192: 17 8 14
-21037: 9 7 18 13
-292: 11 6 16 20
+import itertools
 
-Can the numbers on the left be added or multiplied to equal the number on the right?
-'''
+
+def factors_can_make_target(factors: list, target:int) -> bool:
+    operators = [ '+', '*' ]
+    total_ops = len(factors) - 1
+    op_combos = list(itertools.product(operators, repeat=total_ops))
+
+    for combo in op_combos:
+        expression_parts = [str(factors[0])]
+
+        for i in range(total_ops):
+            expression_parts.append(combo[i])
+            expression_parts.append(str(factors[i + 1]))
+
+        expression = ''.join(expression_parts)
+        if eval(expression) == target:
+            print(f'SUCCESS: {expression} == {target}')
+            return True
+
+    return False
+
+
 def main():
-    pass
 
+    targets = []
+    factors = []
+
+    with open('test.txt', 'r') as file:
+        for line in file:
+            line = line.strip()
+
+            target, factor = line.split(':')
+
+            targets.append(int(target))
+            factors.append(list(map(int, factor.split())))
+
+    print(targets)
+    print(factors)
+    yes = 0
+
+    for i in range(len(targets)):
+        if factors_can_make_target(factors[i], targets[i]):
+            yes += 1
+        else:
+            continue
+
+    print(yes)
 
 if __name__ == '__main__':
     main()

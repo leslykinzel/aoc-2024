7a82b2a4ce72a107f20bb8bb8cee83e968c6aca4
Lesly Kinzel <lesly.helios@proton.me>
2024-12-31 20:38:37 +1100

    We do a little OOP

--- 08/main.py
+++ 08/main.py
@@ -48,9 +48,54 @@
     example.txt contains 14 antinodes.
 
 '''
+from dataclasses import dataclass
+import math
 
 def main():
-    pass
+
+    nodes = list()
+
+    map_width = 0
+    map_height = 0
+
+    with open('example.txt', 'r') as file:
+        for idx_y, line in enumerate(file):
+
+            map_width = len(line)
+            print(line)
+
+            for idx_x in range(len(line)):
+                char = line[idx_x]
+
+                if char == "." or char == "\n":
+                    continue
+
+                nodes.append(Node(char, idx_x, idx_y))
+
+
+        map_height+=1
+
+    print(nodes)
+
+
+@dataclass(frozen=True)
+class Node:
+
+    id: str
+    x: int
+    y: int
+
+    def position(self) -> tuple[int, int]:
+        return tuple(self.x, self.y)
+
+    def get_distance(self, pos: tuple[int, int]) -> int:
+        ''' Calculates distance between this and another Node '''
+        x_diff = self.x - pos[0]
+        y_diff = self.y - pos[1]
+        return x_diff, y_diff
+
+    def __str__(self) -> str:
+        return f'{self.id}: ({self.x}, {self.y})'
 
 
 if __name__ == '__main__':

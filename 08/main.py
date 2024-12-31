63a2cb1113fe012e4ad1b7509ecec65b387d5d25
Lesly Kinzel <lesly.helios@proton.me>
2024-12-31 17:05:33 +1100

    Start day 8.

---
+++ 08/main.py
@@ -0,0 +1,24 @@
+'''
+    ......#....#
+    ...#....0...
+    ....#0....#.
+    ..#....0....
+    ....0....#..
+    .#....A.....
+    ...#........
+    #......#....
+    ........A...
+    .........A..
+    ..........#.
+    ..........#.
+
+    Because the topmost A-frequency antenna overlaps with a 0-frequency antinode, there
+    are 14 total unique locations that contain an antinode within the bounds of the map.
+'''
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()

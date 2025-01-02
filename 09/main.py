30e1388e5698ff3f349e3958ed6e223f55229883
Lesly Kinzel <lesly.helios@proton.me>
2025-01-02 13:20:29 +1100

    encode/decode map strings

--- 09/main.py
+++ 09/main.py
@@ -5,7 +5,16 @@ def main():
             raw = line.strip()
 
     print('Raw input:', raw)
-    print('Decoded:', decode_diskmap(raw))
+
+    decoded = decode_diskmap(raw)
+    print('Decoded:', decoded)
+
+    encoded = encode_diskmap(decoded)
+    print('Encoded:', encoded)
+
+    if encoded == raw:
+        print('success')
+
 
 def decode_diskmap(raw: str) -> str:
     result = ''
@@ -21,6 +30,21 @@ def decode_diskmap(raw: str) -> str:
             # blank space
             for _ in range(int(val)):
                 result += '.'
+
+    return result
+
+def encode_diskmap(map_string: str) -> str:
+    result = ''
+    stack = []
+
+    for val in map_string:
+        if stack and stack[-1] != val:
+            result += f'{len(stack)}{'0' if stack[-1].isnumeric() and val.isnumeric() else ''}'
+            stack.clear()
+        stack.append(val)
+
+    if stack:
+        result += str(len(stack))
 
     return result
 

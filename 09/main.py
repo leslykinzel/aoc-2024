96c98b0d51c763156eaef18c2104558ef2fabac5
Lesly Kinzel <lesly.helios@proton.me>
2025-01-08 00:54:43 +1100

    Complete day 9, part 2.

--- 09/main.py
+++ 09/main.py
@@ -2,22 +2,23 @@ import re
 
 
 def main():
-    with open('example.txt', 'r') as file:
+    with open('input.txt', 'r') as file:
         raw = list(map(int, file.read().strip()))
 
     # print('Raw input:', raw)
 
     decoded = decode_diskmap(raw)
-    print('Decoded:', decoded)
+    # print(decoded)
 
-    # encoded = encode_diskmap(decoded)
-    # print('Encoded:', encoded)
+    part1_defrag = defrag_diskmap(decoded)
+    part1_checksum = calculate_checksum(part1_defrag)
 
-    defrag = defrag_diskmap(decoded)
-    print('Defrag:', defrag)
+    print('Part 1:', part1_checksum)
 
-    checksum = calculate_checksum(defrag)
-    print('Checksum:', checksum)
+    part2_defrag = whole_file_defrag(raw)
+    part2_checksum = calculate_whole_file_checksum(part2_defrag)
+
+    print('Part 2:', part2_checksum)
 
 
 def decode_diskmap(raw: list) -> list:
@@ -43,7 +44,7 @@ def defrag_diskmap(disk: list) -> list:
     '''
         Integers swap positions with periods from right to left.
     '''
-    empty_indexes = [idx for idx, val in enumerate(disk) if val == -1]
+    empty_indexes = [i for i, v in enumerate(disk) if v == -1]
     i = 0
 
     while True:
@@ -57,11 +58,65 @@ def defrag_diskmap(disk: list) -> list:
     return disk
 
 
+def whole_file_defrag(disk: list) -> int:
+    '''
+        Whole file blocks attempt to fill empty spaces starting
+        from idx[-1]. If they can't fill an empty space, move on
+        to the next file block. 
+
+        (file block == group of same int)
+    '''
+    isfile = True
+    files = {}
+    spaces = []
+    ptr = 0
+
+    for i, size in enumerate(disk):
+        if isfile:
+            files[i//2] = (ptr, size)
+        else:
+            spaces.append((ptr, size))
+        isfile = not isfile
+        ptr += size
+
+    for id in reversed(files):
+        loc, file_size = files[id]
+        space_id = 0
+        while space_id < len(spaces):
+            space_loc, space_size = spaces[space_id]
+            if space_loc > loc:
+                break
+            if space_size == file_size:
+                files[id] = (space_loc, file_size)
+                spaces.pop(space_id)
+                break
+            if space_size > file_size:
+                files[id] = (space_loc, file_size)
+                spaces[space_id] = (space_loc + file_size, space_size - file_size)
+                break
+            space_id += 1
+
+    return files
+
+
 def calculate_checksum(disk: list) -> int:
     '''
         Return sum of int[i] * i.
     '''
     return sum(i * val for i, val in enumerate(disk))
+
+
+def calculate_whole_file_checksum(diskmap: dict) -> int:
+    '''
+        Return sum, expecting result of whole_file_defrag
+    '''
+    result = 0
+
+    for id, (loc, size) in diskmap.items():
+        for i in range(loc, loc + size):
+            result += id * i
+
+    return result
 
 
 if __name__ == '__main__':

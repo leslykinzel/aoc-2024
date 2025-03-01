884b6bc586212c845fad1df16c63fc4bf96ef775
Lesly Kinzel <lesly.helios@proton.me>
2025-03-01 12:18:15 +1100

    Update main.py

--- 12/main.py
+++ 12/main.py
@@ -1,6 +1,9 @@
 
 def main():
-    pass
+    with open('input.txt', 'r') as file:
+        data = [list(c.strip()) for c in file.readlines()]
+
+    print(data)
 
 if __name__ == '__main__':
     main()
